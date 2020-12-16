7821ed3bfe22698ff2f8147e1cf66ed2e1fca797
Kauser <jaffari@gmx.de>
2020-12-16 01:06:08 +0100

    completed post

---
+++ src/Feed.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Feed.css';
+import TweetBox from './TweetBox';
+import Post from './Post';
+
+function Feed() {
+    return (
+        <div className="feed">
+            {/* Header */}
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+
+            {/* TweeBox */}
+           <TweetBox/>
+
+
+            {/* Post */}
+            <Post/>
+             {/* Post */}
+              {/* Post */}
+             {/* Post */}
+             {/* Post */}
+              {/* Post */}
+        </div>
+    )
+}
+
+export default Feed
